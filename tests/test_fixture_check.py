aa516c760f3269641f8f3fa586da209e47c581b5
Ubuntu <ubuntu@ip-172-31-38-66.eu-north-1.compute.internal>
2026-07-24 06:39:51 +0000

    removed test_fixture

--- tests/test_fixture_check.py
+++
@@ -1,5 +0,0 @@
-def test_make_auth_event_works(make_auth_event):
-    event = make_auth_event(source_ip="1.2.3.4", auth_result="failed")
-    assert event.source_ip == "1.2.3.4"
-    assert event.auth_result == "failed"
-    assert event.raw == "placeholder raw log line"  # unset field, got the default
